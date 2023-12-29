0b736d294527c617a610725a79ff3ff3d0308c9f
Subham Gourisaria <79411176+SubhamBnk@users.noreply.github.com>
2023-12-29 21:18:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def main():
 
 @app.route("/about")
 def about_page():
-    return "Please subscribe Artificial Intelligence Hub..!!!"
+    return "Please reach out to me on Linkedin!!!"
 
 @app.route("/predict", methods=['GET', 'POST'])
 def upload():
